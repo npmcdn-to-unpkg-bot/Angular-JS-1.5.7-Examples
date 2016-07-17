22fc993f978916362beacbacf0a3b7d6f69209f5
LuisJavier-MontielArenas <luisjavier.montielarenas@gmail.com>
2016-07-17 11:38:11 -0500

    Code it the module part

--- 01 Modules/scripts/own_scripts/angular_scripts/modules/index_module.js
+++ 01 Modules/scripts/own_scripts/angular_scripts/modules/index_module.js
@@ -1,1 +1,6 @@
-alert('I am here!');
+/*
+ * We set a variable to an angular module.
+ * The first parameter that we pass to the module() funtion is the name of the module, in this case indexModule.
+ * The second parameter is an array that can have some configurations, for now we are not going to use them, so we let it empty.
+ */
+var indexModule = angular.module('indexModule', []);
